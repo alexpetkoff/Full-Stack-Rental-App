13e86b2035a5c2023825c06efb784cbbf639cda7
Alexander Petkov <iskander9207@icloud.com>
2025-08-08 17:46:58 +0300

    Navbar

---
+++ client/src/components/Buttons/Button.tsx
@@ -0,0 +1,19 @@
+type ButtonProps = {
+  variant: "outline" | "secondary";
+  children: React.ReactNode;
+};
+
+const Button = ({ variant, children }: ButtonProps) => {
+  const type: Record<string, string> = {
+    outline:
+      "text-center px-4 py-2 text-white hover:text-black bg-transparent hover:bg-white transition-all duration-300 rounded-[8px] ring-1 ring-white font-[600]",
+    secondary:
+      "text-center px-4 py-2 text-white hover:text-black bg-secondary-600 hover:bg-white transition-all duration-300 rounded-[8px] ring-1 ring-secondary-600 hover:ring-white font-[600]",
+  };
+
+  const className = type[variant] ?? "";
+
+  return <div className={className}>{children}</div>;
+};
+
+export default Button;
---
+++ client/src/components/Layout/Navbar.tsx
@@ -0,0 +1,53 @@
+import { NAVBAR_HEIGHT } from "@/lib/constants";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import Button from "../Buttons/Button";
+
+const Navbar = () => {
+  return (
+    <div
+      className="fixed top-0 left-0 w-full z-50 shadow-xl"
+      style={{ height: `${NAVBAR_HEIGHT}px` }}
+    >
+      <div className="flex justify-between items-center w-full py-3 px-8 bg-primary-700">
+        <div className="flex items-center gap-4 md:gap-6">
+          <Link
+            href="/"
+            className="cursor-pointer hover:!text-secondary-300"
+            scroll={false}
+          >
+            <div className="flex items-center gap-3">
+              <Image
+                src="/logo.svg"
+                alt="Rentiful Logo"
+                width={24}
+                height={24}
+                className="w-6 h-6"
+              />
+              <div className="text-xl text-white font-bold">
+                RENT
+                <span className="text-secondary-500 font-light pl-0.5">
+                  IFUL
+                </span>
+              </div>
+            </div>
+          </Link>
+        </div>
+        <p className="text-primary-200 font-[600] hidden md:block">
+          Discover your perfect rental apartment with our advanced search
+        </p>
+        <div className="flex items-center gap-5">
+          <Link href="/signin">
+            <Button variant="outline">Login</Button>
+          </Link>
+          <Link href="/signup">
+            <Button variant="secondary">Register</Button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
