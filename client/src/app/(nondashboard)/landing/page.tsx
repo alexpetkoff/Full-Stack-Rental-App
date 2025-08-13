2d01ed387976c965fa4d87c288b0184c20a7b75b
Alexander Petkov <iskander9207@icloud.com>
2025-08-13 18:02:22 +0300

    Feature cards landing page

--- client/src/app/(nondashboard)/landing/page.tsx
+++ client/src/app/(nondashboard)/landing/page.tsx
@@ -1,8 +1,14 @@
 import React from "react";
 import HeroSection from "./HeroSection";
+import FeaturesSection from "./FeaturesSection";
 
 const Landing = () => {
-  return <HeroSection />;
+  return (
+    <>
+      <HeroSection />
+      <FeaturesSection />
+    </>
+  );
 };
 
 export default Landing;
